13d1050ff99dd23cc7e722986e81d8ae35cab2c6
Selim Oğuz Şahin <selimoguz38@outlook.com>
2022-11-03 13:22:14 +0300

    success

---
+++ BasicMath.c
@@ -0,0 +1,16 @@
+int basic_op(char op, int value1, int value2) {
+    switch(op)
+    {
+        case '+':
+            return (value1 + value2);
+            break;
+        case '-':
+            return (value1 - value2);
+            break;
+        case '*':
+            return (value1 * value2);
+            break;
+        case '/':
+            return (value1 / value2);
+    }
+}
